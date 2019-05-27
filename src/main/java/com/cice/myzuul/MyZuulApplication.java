bba859d26d19c8b062f33162c1504ac4c4e7a501
unknown <cice@A3P05S.CICE.LOCAL>
2019-05-27 20:52:10 +0200

    Initial commit

---
+++ src/main/java/com/cice/myzuul/MyZuulApplication.java
@@ -0,0 +1,17 @@
+package com.cice.myzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+public class MyZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyZuulApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cice/myzuul/Serverzuul.java
@@ -0,0 +1,18 @@
+package com.cice.myzuul;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Serverzuul {
+
+
+    @Test
+    public void contestload(){
+
+    }
+}
